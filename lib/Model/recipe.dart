05fbc9a125bd4ec34a6dee298dcd4eca4b173b9c
Eittipakorn <eittipakorn.sun@gmail.com>
2025-12-19 23:00:41 +0700

    Add ingredient model class

--- lib/Model/recipe.dart
+++ lib/Model/recipe.dart
@@ -1,16 +1,73 @@
+import 'package:recipes/Model/ingredient.dart';
+
 class Recipe {
   String imgLabel;
   String imageUrl;
   String imgDesc;
+  List<Ingredient> ingredients;
 
-  Recipe(this.imageUrl, this.imgLabel, this.imgDesc);
+  Recipe(this.imageUrl, this.imgLabel, this.imgDesc, this.ingredients);
 
   static List<Recipe> samples = [
-    Recipe('assets/images/Aubergine Thai jungle curry.webp', 'Aubergine Thai jungle curry', 'This aubergine Thai jungle curry is low-calorie and ready in just 20 minutes, making a great, satisfying option for midweek.'),
-    Recipe('assets/images/Crying tiger beef salad (yum nua seua rong hai).webp', 'Crying tiger beef salad (yum nua seua rong hai)', 'Enjoy the bold, spicy and tangy flavours of crying tiger beef salad, a Thai special-occasion dish. Tender grilled sirloin sits on crisp lettuce, dressed with a zesty lime and chilli sauce.'),
-    Recipe('assets/images/Jackfruit massaman curry.webp', 'Jackfruit massaman curry', 'A tin of jackfruit (which you can pick up at almost any supermarket) is the magic ingredient in this vegan massaman curry, served with salted peanuts.'),
-    Recipe('assets/images/Larb muang (Thai chopped pork salad).webp', 'Larb muang (Thai chopped pork salad)', 'Experience bold Thai flavours with Larb Muang, a vibrant chopped pork salad bursting with garlic, crispy shallots and spiced chillies. Perfect for sharing, this easy dish is served hot with sticky rice or lettuce cups.'),
-    Recipe('assets/images/Pad Thai.webp', 'Pad Thai', 'Put juicy king prawns to work in our version of this classic Thai dish, full of vibrant flavours and ready in half an hour.'),
+    Recipe('assets/images/Aubergine Thai jungle curry.webp', 'Aubergine Thai jungle curry', 'This aubergine Thai jungle curry is low-calorie and ready in just 20 minutes, making a great, satisfying option for midweek.',
+    [
+      Ingredient(name: 'Aubergines', quantity: 2, unit: ''),
+      Ingredient(name: 'Jungle curry paste', quantity: 2, unit: 'tbsp'),
+      Ingredient(name: 'Vegetable stock', quantity: 2, unit: 'cups'),
+      Ingredient(name: 'Kaffir lime leaves', quantity: 3, unit: ''),
+      Ingredient(name: 'Thai chilies', quantity: 2, unit: ''),
+      Ingredient(name: 'Fish sauce', quantity: 1, unit: 'tbsp'),
+      Ingredient(name: 'Thai basil', quantity: 1, unit: 'handful'),
+    ]
+    ),
+    
+    Recipe('assets/images/Crying tiger beef salad (yum nua seua rong hai).webp', 'Crying tiger beef salad (yum nua seua rong hai)', 'Enjoy the bold, spicy and tangy flavours of crying tiger beef salad, a Thai special-occasion dish. Tender grilled sirloin sits on crisp lettuce, dressed with a zesty lime and chilli sauce.',
+    [
+      Ingredient(name: 'Aubergines', quantity: 2, unit: ''),
+      Ingredient(name: 'Jungle curry paste', quantity: 2, unit: 'tbsp'),
+      Ingredient(name: 'Vegetable stock', quantity: 2, unit: 'cups'),
+      Ingredient(name: 'Kaffir lime leaves', quantity: 3, unit: ''),
+      Ingredient(name: 'Thai chilies', quantity: 2, unit: ''),
+      Ingredient(name: 'Fish sauce', quantity: 1, unit: 'tbsp'),
+      Ingredient(name: 'Thai basil', quantity: 1, unit: 'handful'),
+    ]
+    ),
+
+    Recipe('assets/images/Jackfruit massaman curry.webp', 'Jackfruit massaman curry', 'A tin of jackfruit (which you can pick up at almost any supermarket) is the magic ingredient in this vegan massaman curry, served with salted peanuts.',
+    [
+      Ingredient(name: 'Aubergines', quantity: 2, unit: ''),
+      Ingredient(name: 'Jungle curry paste', quantity: 2, unit: 'tbsp'),
+      Ingredient(name: 'Vegetable stock', quantity: 2, unit: 'cups'),
+      Ingredient(name: 'Kaffir lime leaves', quantity: 3, unit: ''),
+      Ingredient(name: 'Thai chilies', quantity: 2, unit: ''),
+      Ingredient(name: 'Fish sauce', quantity: 1, unit: 'tbsp'),
+      Ingredient(name: 'Thai basil', quantity: 1, unit: 'handful'),
+    ]
+    ),
+
+    Recipe('assets/images/Larb muang (Thai chopped pork salad).webp', 'Larb muang (Thai chopped pork salad)', 'Experience bold Thai flavours with Larb Muang, a vibrant chopped pork salad bursting with garlic, crispy shallots and spiced chillies. Perfect for sharing, this easy dish is served hot with sticky rice or lettuce cups.',
+    [
+      Ingredient(name: 'Aubergines', quantity: 2, unit: ''),
+      Ingredient(name: 'Jungle curry paste', quantity: 2, unit: 'tbsp'),
+      Ingredient(name: 'Vegetable stock', quantity: 2, unit: 'cups'),
+      Ingredient(name: 'Kaffir lime leaves', quantity: 3, unit: ''),
+      Ingredient(name: 'Thai chilies', quantity: 2, unit: ''),
+      Ingredient(name: 'Fish sauce', quantity: 1, unit: 'tbsp'),
+      Ingredient(name: 'Thai basil', quantity: 1, unit: 'handful'),
+    ]
+    ),
+
+    Recipe('assets/images/Pad Thai.webp', 'Pad Thai', 'Put juicy king prawns to work in our version of this classic Thai dish, full of vibrant flavours and ready in half an hour.',
+    [
+      Ingredient(name: 'Aubergines', quantity: 2, unit: ''),
+      Ingredient(name: 'Jungle curry paste', quantity: 2, unit: 'tbsp'),
+      Ingredient(name: 'Vegetable stock', quantity: 2, unit: 'cups'),
+      Ingredient(name: 'Kaffir lime leaves', quantity: 3, unit: ''),
+      Ingredient(name: 'Thai chilies', quantity: 2, unit: ''),
+      Ingredient(name: 'Fish sauce', quantity: 1, unit: 'tbsp'),
+      Ingredient(name: 'Thai basil', quantity: 1, unit: 'handful'),
+    ]
+    ),
   ];
 
   get imgTitle => null;
--- lib/recipe-detail.dart
+++ lib/recipe-detail.dart
@@ -1,34 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:recipes/Model/recipe.dart';
 
-class RecipeDetail extends StatelessWidget{
-  Recipe recipe;
-
+class RecipeDetail extends StatefulWidget{
+  final Recipe recipe;
+  int _sliderVal = 1;
   RecipeDetail({super.key, required this.recipe});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Recipe Detail'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Image.asset(recipe.imageUrl),
+  State<StatefulWidget> createState() {
+    return RecipeDetailState();
+  }
+    
+}
+  
+
+class RecipeDetailState extends State<RecipeDetail> {
+  int sliderVal = 1;
+
+  @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+      appBar: AppBar(title: const Text('Recipe Detail')),
+      body: Column(
+        children: [
+            Image.asset(widget.recipe.imageUrl),
             const SizedBox(height: 20),
             Text(
-              recipe.imgLabel,
+              widget.recipe.imgLabel,
               style: const TextStyle(fontSize: 24, fontFamily: 'Athiti', fontWeight: FontWeight.bold),
             ),
             Text(
-              recipe.imgDesc,
+              widget.recipe.imgDesc,
               style: const TextStyle(fontSize: 16),
             ),
-          ],
-        ),
+
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.builder(itemBuilder: (BuildContext context, int index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  final adjustedQuantity = widget.recipe.ingredients[index].quantity * sliderVal;
+                  return Text(
+                    '${adjustedQuantity * widget._sliderVal} ${ingredient.unit} ${ingredient.name}',
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  );
+                },itemCount: widget.recipe.ingredients.length,),
+              ),
+            ),
+
+            Slider(
+              min: 1,
+              max: 10,
+              divisions: 10,
+              label: '$sliderVal servings',
+              value: sliderVal.toDouble(),
+
+              onChanged: (newValue) {
+                setState(() {
+                  sliderVal = newValue.round();
+                });
+              },
+            ),
+
+        ],
+        
       ),
     );
   }
-  
 }
